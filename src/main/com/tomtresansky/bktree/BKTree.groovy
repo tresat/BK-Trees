948bb5da6c232e4c17a0fe1f96598dac2628a782
tomwork <ttresans@courts.state.ny.us>
2013-05-28 13:05:23 -0400

    Working trees, possibly.
    -Added simple tests for finder.

--- src/main/com/tomtresansky/bktree/BKTree.groovy
+++ src/main/com/tomtresansky/bktree/BKTree.groovy
@@ -1,36 +1,71 @@
 package com.tomtresansky.bktree
 
+
+/**
+ * See http://java.dzone.com/articles/algorithm-week-bk-trees-part-1
+ *
+ * @author tom
+ */
 class BKTree {
-    BKNode root
+  BKNode root
+  LevenshteinDistanceFinder df = new LevenshteinDistanceFinder()
 
-    private static class BKNode {
-        String word
-        Map children = [:]
+  private static class BKNode {
+    String word
+    Map children = [:]
 
-        BKNode(String word) {
-            this.word = word
-        }
+    @Override
+    public String toString() {
+      return word
     }
+  }
 
-    BKTree(List dictionary) {
-        assert dictionary.size() >= 1
+  BKTree(List dictionary) {
+    assert dictionary.size() >= 1
 
-        root = new BKNode(dictionary[0])
-        dictionary[1..-1].each { w -> insertWord(w) }
+    root = new BKNode(word:dictionary[0])
+    dictionary[1..-1].each { w -> insertWord(w) }
+  }
+
+  void insertWord(String newWord) {
+    insertWordRec(root, newWord)
+  }
+
+  private void insertWordRec(BKNode node, String newWord) {
+    def distance = df.findLevenshteinDistance(root.word, newWord)
+    // println "Distance between $root.word and $newWord is $distance"
+
+    if (node.children.containsKey(distance)) {
+      insertWordRec(node.children[distance], newWord)
+    } else {
+      node.children[distance] = new BKNode(word:newWord)
+      // println "Inserted $newWord under $root.word"
     }
+  }
 
-    void insertWord(String word) {
-        LevenshteinDistanceFinder df = new LevenshteinDistanceFinder()
+  void print() {
+    printRec(root)
+  }
 
-        df.findLevenshteinDistance
-    }
+  private void printRec(BKNode node) {
+    println "$node.word -> ${ node.children.each { c -> c.value } }"
+    node.children.each { c -> printRec(c.value) }
+  }
 
-    void print() {
-        printRec(root)
-    }
 
-    void printRec(BKNode node) {
-        println "$node.word -> ${ node.children.each { c -> c.word } }"
-        node.children.each { c -> printRec(c) }
-    }
+  static main(String... args) {
+    def dictionary = [
+      'Some',
+      'Awesome',
+      'Words',
+      'Go',
+      'Here',
+      'Book',
+      'Rook',
+      'Hooks'
+    ]
+
+    def tree = new BKTree(dictionary)
+    tree.print()
+  }
 }
--- src/test/com/tomtresansky/bktree/LevenshteinDistanceFinderTest.groovy
+++ src/test/com/tomtresansky/bktree/LevenshteinDistanceFinderTest.groovy
@@ -1,6 +1,37 @@
 package com.tomtresansky.bktree
 
+import spock.lang.Specification
 
-class LevenshteinDistanceFinderTest {
+class LevenshteinDistanceFinderTest extends Specification {
+  final LevenshteinDistanceFinder df = new LevenshteinDistanceFinder()
 
+  def "test recursive find method"() {
+    expect:
+    df.findLevenshteinDistanceRec(s1, s2) == r
+
+    where:
+    r | s1          | s2
+    3 | "kitten"    | "sitting"
+    3 | "Saturday"  | "Sunday"
+  }
+
+  def "test full iterative find method"() {
+    expect:
+    df.findLevenshteinDistanceFullIterative(s1, s2) == r
+
+    where:
+    r | s1          | s2
+    3 | "kitten"    | "sitting"
+    3 | "Saturday"  | "Sunday"
+  }
+
+  def "test default quick iterative find method"() {
+    expect:
+    df.findLevenshteinDistance(s1, s2) == r
+
+    where:
+    r | s1          | s2
+    3 | "kitten"    | "sitting"
+    3 | "Saturday"  | "Sunday"
+  }
 }
