1f132e20f56c3e663dff2e43f06abebdcf376e38
Tom Tresansky <tom.tresansky@gmail.com>
2013-05-29 17:51:41 -0400

    Working BK Trees.

--- src/main/com/tomtresansky/bktree/BKTree.groovy
+++ src/main/com/tomtresansky/bktree/BKTree.groovy
@@ -23,23 +23,27 @@ class BKTree {
   BKTree(List dictionary) {
     assert dictionary.size() >= 1
 
+    println "Creating tree rooted at ${dictionary[0]}"
     root = new BKNode(word:dictionary[0])
     dictionary[1..-1].each { w -> insertWord(w) }
   }
 
   void insertWord(String newWord) {
+    println "Inserting $newWord"
     insertWordRec(root, newWord)
   }
 
   private void insertWordRec(BKNode node, String newWord) {
-    def distance = df.findLevenshteinDistance(root.word, newWord)
-    // println "Distance between $root.word and $newWord is $distance"
+    def distance = df.findLevenshteinDistance(node.word, newWord)
+    println "Distance between $node.word and $newWord is $distance"
 
     if (node.children.containsKey(distance)) {
+      println "Following edge $distance (${node.children[distance]})"
       insertWordRec(node.children[distance], newWord)
     } else {
+      println "Inserting $newWord under $node.word at $distance"
       node.children[distance] = new BKNode(word:newWord)
-      // println "Inserted $newWord under $root.word"
+      print()
     }
   }
 
@@ -55,14 +59,10 @@ class BKTree {
 
   static main(String... args) {
     def dictionary = [
-      'Some',
-      'Awesome',
-      'Words',
-      'Go',
-      'Here',
-      'Book',
-      'Rook',
-      'Hooks'
+      'book',
+      'rook',
+      'nooks',
+      'boon'
     ]
 
     def tree = new BKTree(dictionary)
